2589a6b71eea281539ded3480c5cd1541fc75ac1
dergaxhtml <dergax.s3@gmail.com>
2021-01-17 23:10:49 +0100

    init+hello World

---
+++ api/src/test/java/com/pdt/ihlt/IhltApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pdt.ihlt;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IhltApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
